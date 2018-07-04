653ceccb3626457bace0893d1c6ccdac74c5dce9
Colby Guan <colbyguan@gmail.com>
2018-07-03 18:35:28 -0700

    react-ified front page with load-from-json

---
+++ src/content.js
@@ -0,0 +1,138 @@
+const content = {
+  name: 'Vivien Bui',
+  tagline: 'Writer. Baldie. Jobless',
+  blurb: 'I am vivien. I am a bui',
+  sections: [
+    {
+      title: 'creative non-fiction',
+      /* lower order equals a higher spot on the page */
+      order: 2,
+      /* available colors: red, orange, blue, defaults to green */
+      color: 'green',
+      description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp',
+      showFirstItems: 3,
+      items: [
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        /* Don't forget commas between each item */
+        {
+          title: 'Promolta',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Entro',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        }
+      ]
+    },
+    {
+      title: 'fiction',
+      color: 'red',
+      order: 3,
+      description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp',
+      showFirstItems: 3,
+      items: [
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Promolta',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Entro',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+      ]
+    },
+    {
+      title: 'professional',
+      order: 1,
+      color: 'blue',
+      description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp',
+      showFirstItems: 3,
+      items: [
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Promolta',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Entro',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+      ]
+    },
+    {
+      title: 'poetry',
+      order: 4,
+      color: 'orange',
+      description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp',
+      showFirstItems: 3,
+      items: [
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Promolta',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'Entro',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+        {
+          title: 'VIA 1',
+          link: 'http://via1.org/via-1-2018/',
+          description: 'Because TS adds an extra layer of tooling, its one more thing you need to work into your JS workflow. If youre already comfortable with things like Gulp'
+        },
+      ]
+    },
+  ]
+};
+
+export default content;
